2a543c97b52a955307df0bde0a2bca5920dca40a
tranquocthinh88 <97369919+tranquocthinh88@users.noreply.github.com>
2024-11-19 11:04:24 +0700

    change temp_f to temp_c

--- src/main/java/com/code/function_calling/dto/Weather.java
+++ src/main/java/com/code/function_calling/dto/Weather.java
@@ -5,5 +5,5 @@ public class Weather {
     public record Request (String city){}
     public record Response (Location location, Current current){}
     public record Location (String name, String country){}
-    public record Current (String temp_f){}
+    public record Current (String temp_c){}
 }
